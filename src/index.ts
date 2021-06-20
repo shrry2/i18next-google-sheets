615643130ba9414df60a43691918db99d913bdc4
Takaki Takeuchi <takaki@takeu.ch>
2021-06-20 19:38:30 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers';
+
+yargs(hideBin(process.argv))
+  .command(
+    'curl <url>',
+    'fetch the contents of the URL',
+    () => {},
+    argv => {
+      console.info(argv);
+    }
+  )
+  .demandCommand(1).argv;
